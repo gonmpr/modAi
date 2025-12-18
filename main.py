1e7b35e2fad66bdb62aaef0cd039112fcb41021e
¨gonmpr¨ <gmporrorosas@gmail.com>
2025-12-18 20:46:30 -0300

    api added

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get('GEMINI_API_KEY')
+
+if not api_key:
+    raise RuntimeError('API KEY NOT FOUND')
+
+client = genai.Client(api_key = api_key)
+prompt ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
 def main():
-    print("Hello from modai!")
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=prompt
+    )
+    print(response.text)
 
 
 if __name__ == "__main__":
