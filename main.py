beb2736ccdd536405614d50f973f710a3319867c
¨gonmpr¨ <gmporrorosas@gmail.com>
2025-12-19 15:14:33 -0300

    handle input and output

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
-
 from google import genai
 
 
-load_dotenv()
-api_key = os.environ.get('GEMINI_API_KEY')
 
-if not api_key:
-    raise RuntimeError('API KEY NOT FOUND')
 
-client = genai.Client(api_key = api_key)
-prompt ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+def get_response(client, prompt):
 
-def main():
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=prompt
+        contents=prompt,
     )
-    print(response.text)
 
+    if response.usage_metadata is None:
+        raise RuntimeError('FAILED API REQUEST: get_response() function error')
+
+    response_and_meta = {'response': response.text,
+                         'prompt_tokens': response.usage_metadata.prompt_token_count,
+                         'response_tokens': response.usage_metadata.candidates_token_count,
+                        }
+    return response_and_meta
+
+
+
+
+
+
+
+def main():
+
+    load_dotenv()
+    api_key = os.environ.get('GEMINI_API_KEY')
+    if not api_key:
+        raise RuntimeError('API KEY NOT FOUND')
+
+    client = genai.Client(api_key=api_key)
+
+
+    input_parser = argparse.ArgumentParser(description='modAi is a agentic Ai that screw up your proyects')
+    input_parser.add_argument('user_prompt', type=str, help='expecting user prompt')
+    args = input_parser.parse_args()
+    prompt = args.user_prompt
+
+    response = get_response(client,prompt)
+
+    print('User prompt:', prompt)
+    print('Prompt tokens:',response['prompt_tokens'])
+    print('Response tokens:',response['response_tokens'])
+    print('Response:',response['response'])
 
 if __name__ == "__main__":
     main()
